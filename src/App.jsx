fcd77e35e6a76140d47005770b7c55eea864f3d8
Staroyee <danmasefield@gmail.com>
2024-02-02 23:39:27 +1030

    Router fix
    previous fix didn't work, trying another tactic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
 import Footer from "./Components/Footer/Footer.jsx";
 import "./App.css";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
 const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
