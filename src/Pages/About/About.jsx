ee81de0484807d84f6c7c575d804d2e8771c8d17
Staroyee <danmasefield@gmail.com>
2024-02-03 21:13:24 +1030

    Create About Page
    Added content to about page
    Styled about page
    Added responsiveness to about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,34 @@
+import Coffee from "../../assets/images/coffee.jpg";
 import "./about.css";
 
 const About = () => {
   return (
     <>
-      <section id="about" className="home-container">
-        <h1 className="page-title">ABOUT</h1>
+      <section id="about" className="about-container">
+        <img src={Coffee} alt="" className="the-vault-image" />
+        <div>
+          <h1 className="about-title">The Vault at Verdun</h1>
+          <div className="text-container">
+            <p>
+              Welcome to The Vault @ Verdun Adelaide Hill’s newest Cafe & Co-op
+              space.
+            </p>
+            <p>
+              Open now, find us in the beautiful town of Verdun, just two
+              minutes beyond the Hahndorf freeway exit.
+            </p>
+            <p>
+              Set in a converted 1930’s church on the Main Street, The Vault has
+              kept its heritage charm. Natural light floods through the original
+              stain glass windows and vaulted 5 metre ceilings showcasing ornate
+              beams towering above.
+            </p>
+            <p>
+              Tell your family, tell your friends, there’s a new treasure in the
+              Adelaide Hills at 33 Onkaparinga Valley Rd Verdun.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
