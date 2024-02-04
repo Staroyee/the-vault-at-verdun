2259b9a5eb7da545f023336468a2f9e64b49d44c
Staroyee <danmasefield@gmail.com>
2024-02-04 11:21:37 +1030

    Test routes fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from "./Pages/Home/Home.jsx";
-import About from "./Pages/About/About.jsx";
-import Collective from "./Pages/Collective/Collective.jsx";
-import Cafe from "./Pages/Cafe/Cafe.jsx";
+import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer/Footer.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="*" index element={<Home />} />
-          <Route path="/collective" element={<Collective />} />
-          <Route path="/cafe" element={<Cafe />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <Outlet />
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import About from "./Pages/About/About.jsx";
+import Collective from "./Pages/Collective/Collective.jsx";
+import Cafe from "./Pages/Cafe/Cafe.jsx";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/collective",
+        element: <Collective />,
+      },
+      {
+        path: "/cafe",
+        element: <Cafe />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
